5818d7356ed977ef8440ed1ed0056430e55dc870
Uberacy Tottoli <bera@boldtech.com.br>
2020-01-18 18:08:17 -0300

    Adiciona FilmeController e FilmeDTO

---
+++ ConsumidorDeFilmes/Filmes.Api/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        // GET: api/Filme
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ ConsumidorDeFilmes/Filmes.Api/Models/FilmeDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmes.Api.Models
+{
+    public class FilmeDTO
+    {
+    }
+}
